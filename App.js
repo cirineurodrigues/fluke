a9cbb25c9d19ca4dbfc332e819d29b77686a7055
Cirineu Rodrigues <cirineurodrigues@hotmail.com>
2021-02-10 11:53:01 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Olá, Fluke!</Text>
+    </View>
+  );
+};
+
+export default App;
